2b88e1251f9a59b3524fc03a97b5721c3a4c4241
avilac26 <emanuelavilac@gmail.com>
2026-01-16 19:13:22 -0600

    Corrected footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,13 +9,15 @@ function Footer(){
         <div className="Footer">
             <hr/>
             <div className="FooterContainer">
-                <nav>
-                    <ul className="FooterOptionsContainer">
-                        <li className="navOption"> <Link to={"/"}>Home</Link> </li>
-                        <li className="navOption"> <Link to={"/about"}>About</Link> </li>
-                        <li className="navOption"> <Link to={"/projects"}>Projects</Link> </li>
-                    </ul>
-                </nav>
+                <div className="FooterOptionsContainer1">
+                    <nav>
+                        <ul className="FooterOptionsContainer">
+                            <li className="navOption"> <Link to={"/"}>Home</Link> </li>
+                            <li className="navOption"> <Link to={"/about"}>About</Link> </li>
+                            <li className="navOption"> <Link to={"/projects"}>Projects</Link> </li>
+                        </ul>
+                    </nav>
+                </div>
                 <div className="FooterAuthorInfoContainer">
                     <p>@2025 Emanuel Ávila Córdoba dev. All rights reserved.</p>
                 </div>
